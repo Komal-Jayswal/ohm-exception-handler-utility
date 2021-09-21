d5dea3d122256aaa55ba851180882ed0b5e66a0e
Tripti-Sinha-maersk <tripti.sinha@maersk.com>
2021-09-21 12:51:42 +0530

    added ViolationException class

---
+++ src/main/java/net/apmoller/crb/ohp/microservices/exception/ViolationException.java
@@ -0,0 +1,9 @@
+package net.apmoller.crb.ohp.microservices.exception;
+
+
+public class ViolationException extends Exception {
+
+    public ViolationException(String message) {
+        super(message);
+    }
+}
